3175feda6b47c1c656bca4e1de930c9536873dcb
Hemanth4785 <bhemachandran2005@gmail.com>
2026-01-19 21:11:36 +0530

    mapping

--- services/apiService.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = "http://192.168.0.105";
-
-export const testBackend = async () => {
-  const response = await axios.get(`${BASE_URL}/test`);
-  return response.data;
-};
