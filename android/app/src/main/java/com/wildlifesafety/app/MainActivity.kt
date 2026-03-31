bdd5bb7a1b9a0d4d5e9d8b9719ad8856cb3e24a8
Hemanth4785 <bhemachandran2005@gmail.com>
2026-03-31 09:18:42 +0530

    Sync native directory and fix corrupted image assets

--- android/app/src/main/java/com/wildlifesafety/app/MainActivity.kt
+++ android/app/src/main/java/com/wildlifesafety/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.wildlifesafety
+package com.wildlifesafety.app
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/wildlifesafety/app/MainApplication.kt
+++ android/app/src/main/java/com/wildlifesafety/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.wildlifesafety
+package com.wildlifesafety.app
 
 import android.app.Application
 import android.content.res.Configuration
